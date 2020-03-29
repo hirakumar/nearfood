ceeec566f07661bb2c31ee12ca480193a6f155ce
hirakumarolive <hirakumarmaharjan@olivemedia.co>
2020-03-29 10:00:04 +0545

    npm start is executed

---
+++ dist/precache-manifest.e2aed4a6a5b818313661898283a50d47.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0fb613eb76204da159",
+    "url": "/css/about.15ab8253.css"
+  },
+  {
+    "revision": "afc833034aa9f33d792b",
+    "url": "/css/app.1c9e55b1.css"
+  },
+  {
+    "revision": "6bfd1b4b749f52a2143f",
+    "url": "/css/chunk-vendors.31a5bb6c.css"
+  },
+  {
+    "revision": "ee028c1d938ef8f272e979afb5fbb8a7",
+    "url": "/img/truck.ee028c1d.jpg"
+  },
+  {
+    "revision": "1cf21a8e274683d6114594d6b07916c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a0fb613eb76204da159",
+    "url": "/js/about.2e97dade.js"
+  },
+  {
+    "revision": "afc833034aa9f33d792b",
+    "url": "/js/app.22bee23b.js"
+  },
+  {
+    "revision": "6bfd1b4b749f52a2143f",
+    "url": "/js/chunk-vendors.067eae25.js"
+  },
+  {
+    "revision": "3fea8d9580b9cd1473f6f8f9f0167103",
+    "url": "/location.png"
+  },
+  {
+    "revision": "a260a764fd4d2e274812db672c8a6c78",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "986453c2f31da5fe7e73c3e093a0e608",
+    "url": "/serving-dish-bw.png"
+  },
+  {
+    "revision": "f2928ab683c3939a844cb313d4836e29",
+    "url": "/serving-dish.png"
+  }
+]);
